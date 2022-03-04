fc98da6888b323d91697423739ee7f510ee92a81
aimeeyourell20 <61693747+aimeeyourell20@users.noreply.github.com>
2022-03-04 18:35:16 +0000

    adminLogin() & customerLogin()
    The last two methods created from the menuStart()

--- SoftwarePatternsRefactoringAssignment/Menu.java
+++ SoftwarePatternsRefactoringAssignment/Menu.java
@@ -375,6 +375,140 @@ public class Menu extends JFrame{
 			
 		}
 
+		
+		//Allows  admin to login with their username
+		public void adminLogin() {
+			
+			boolean loop = true, loop2 = true;
+			boolean cont = false;
+		    while(loop)
+		    {
+		    Object adminUsername = JOptionPane.showInputDialog(frame, "Enter Administrator Username:");
+
+		    if(!adminUsername.equals("admin"))//search admin list for admin with matching admin username
+		    {
+		    	int reply  = JOptionPane.showConfirmDialog(null, null, "Incorrect Username. Try again?", JOptionPane.YES_NO_OPTION);
+		    	if (reply == JOptionPane.YES_OPTION) {
+		    		loop = true;
+		    	}
+		    	else if(reply == JOptionPane.NO_OPTION)
+		    	{
+		    		frame1.dispose();
+		    		loop = false;
+		    		loop2 = false;
+		    		menuStart();
+		    	}
+		    }
+		    else
+		    {
+		    	loop = false;
+		    }				    
+		    }
+		    
+		    while(loop2)
+		    {
+		    	Object adminPassword = JOptionPane.showInputDialog(frame, "Enter Administrator StrPassword;");
+		    	
+		    	   if(!adminPassword.equals("admin11"))//search admin list for admin with matching admin StrPassword
+				    {
+				    	int reply  = JOptionPane.showConfirmDialog(null, null, "Incorrect StrPassword. Try again?", JOptionPane.YES_NO_OPTION);
+				    	if (reply == JOptionPane.YES_OPTION) {
+				    		
+				    	}
+				    	else if(reply == JOptionPane.NO_OPTION){
+				    		frame1.dispose();
+				    		loop2 = false;
+				    		menuStart();
+				    	}
+				    }
+		    	   else
+		    	   {
+		    		   loop2 =false;
+		    		   cont = true;
+		    	   }
+		    }
+		    	
+		    if(cont)
+		    {
+			frame1.dispose();
+		    loop = false;
+		    admin();					    
+		    }					    
+		
+			
+		}
+		
+		//Allows customer to login
+		public void customerLogin() {
+			
+			boolean loop = true, loop2 = true;
+			boolean cont = false;
+			boolean found = false;
+			Customer customer = null;
+		    while(loop)
+		    {
+		    Object customerID = JOptionPane.showInputDialog(frame, "Enter Customer ID:");
+		    
+		    for (Customer aCustomer: customerList){
+		    	
+		    	if(aCustomer.getCustomerID().equals(customerID))//search customer list for matching customer ID
+		    	{
+		    		found = true;
+		    		customer = aCustomer;
+		    	}					    	
+		    }
+		    
+		    if(found == false)
+		    {
+		    	int reply  = JOptionPane.showConfirmDialog(null, null, "User not found. Try again?", JOptionPane.YES_NO_OPTION);
+		    	if (reply == JOptionPane.YES_OPTION) {
+		    		loop = true;
+		    	}
+		    	else if(reply == JOptionPane.NO_OPTION)
+		    	{
+		    		frame.dispose();
+		    		loop = false;
+		    		loop2 = false;
+		    		menuStart();
+		    	}
+		    }
+		    else
+		    {
+		    	loop = false;
+		    }
+		   
+		    }
+		    
+		    while(loop2)
+		    {
+		    	Object customerPassword = JOptionPane.showInputDialog(frame, "Enter Customer StrPassword;");
+		    	
+		    	   if(!customer.getPassword().equals(customerPassword))//check if custoemr StrPassword is correct
+				    {
+				    	int reply  = JOptionPane.showConfirmDialog(null, null, "Incorrect StrPassword. Try again?", JOptionPane.YES_NO_OPTION);
+				    	if (reply == JOptionPane.YES_OPTION) {
+				    		
+				    	}
+				    	else if(reply == JOptionPane.NO_OPTION){
+				    		frame.dispose();
+				    		loop2 = false;
+				    		menuStart();
+				    	}
+				    }
+		    	   else
+		    	   {
+		    		   loop2 =false;
+		    		   cont = true;
+		    	   }
+		    }
+		    	
+		    if(cont)
+		    {
+			frame.dispose();
+		    	loop = false;
+		    	customer(customer);				    
+		    }				    	
+		}
 	
 	public void admin()
 	{
